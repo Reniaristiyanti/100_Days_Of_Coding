892a252fb63096ac5565e29970bf619e63eb552e
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-10 16:10:25 +0800

    Day2

---
+++ Day2.java
@@ -0,0 +1,8 @@
+package ISC;
+
+public class Day2 {
+    public static void main(String[] args) {
+      String nama = "Reni Aristiyanti";
+        System.out.println("Perkenalkan nama saya :" + nama);
+    }
+}
