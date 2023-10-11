d969eb5b5c7eae6d1b61821e4e8109d586cb3bee
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-11 20:33:39 +0800

    Day3

---
+++ Day3.java
@@ -0,0 +1,22 @@
+package ISC;
+
+public class Day3 {
+    public static void main(String[] args) {
+        //Deklarasi variabel menggunakan tipe data primitif
+        int umur = 20;
+        float TinggiBadan = 156.0f;
+        double BeratBadan = 51.0;
+        char JenisKelamin = 'P';
+        boolean Mahasiswa = true;
+        byte JumlahSaudara = 5;
+        short UkuranSepatu = 38;
+        
+        System.out.println("Umur saya saat ini adalah: " + umur + "Tahun");
+        System.out.println("Tinggi badan saya yaitu: " + TinggiBadan);
+        System.out.println("Berat badan saya: " + BeratBadan);
+        System.out.println("Jenis kelamin saya: " + JenisKelamin);
+        System.out.println("Saat ini saya berstatus sebagai mahasiswa di unsulbar: " + Mahasiswa);
+        System.out.println("JUmlah saudara saya ada : " + JumlahSaudara);
+        System.out.println("Ukuran sepatu saya yaitu: " + UkuranSepatu);
+    }
+}
