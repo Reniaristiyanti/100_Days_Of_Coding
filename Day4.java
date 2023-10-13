8b3086d055e2a5d9993324165b0e255f50c1162d
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-13 10:43:52 +0800

    Day4

---
+++ Day4.java
@@ -0,0 +1,18 @@
+package ISC;
+
+public class Day4 {
+    public static void main(String[] args) {
+        //Mengkonversi String ke tipe data primitif
+        
+        //Deklarasi variabel String
+        String angka = "12345";
+        
+        //Konversi String ke tipe data integer
+        int bilangan = Integer.parseInt(angka);
+        
+        System.out.println("Hasil dari konversi variabel String ke integer ");
+        System.out.println("Bilangan: " + angka);
+        
+    }
+    
+}
