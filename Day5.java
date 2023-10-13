07f2aaf3c36a45d302f1f30335ed59f022886934
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-13 11:21:09 +0800

    Day5

--- Day5.java
+++ Day5.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ISC;
 
-/**
- *
- * @author renis
- */
 public class Day5 {
+    public static void main(String[] args) {
+        //Mengkonversi Tipe data primitif ke String
+        
+        //Deklarasi variabel tipe data primitif
+        int umur = 20;
+        short BeratBadan = 51;
+        char JenisKelamin = 'P';
+        boolean Mahasiswa = true;
+        
+        //Konversi tipe data primitif ke String
+        String strUmur = String.valueOf(umur);
+        String strBeratbadan = String.valueOf(BeratBadan);
+        String strJeniskelamin = String.valueOf(JenisKelamin);
+        String strmahasiswa = String.valueOf(Mahasiswa);
+        
+        System.out.println("Umur: " + strUmur);
+        System.out.println("Berat badan: " + strBeratbadan);
+        System.out.println("Jenis kelamin: " + strJeniskelamin);
+        System.out.println("Mahasiswa: " + strmahasiswa);
+    }
     
 }
