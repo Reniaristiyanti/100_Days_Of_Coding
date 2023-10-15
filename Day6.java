6498ac4fe857be47f9d4e91f832b0bc6c3158fe8
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-15 19:07:46 +0800

    Day6

---
+++ Day6.java
@@ -0,0 +1,28 @@
+package ISC;
+
+public class Day6 {
+    public static void main(String[] args) {
+        // Variabel untuk menampung hari
+        int hari = 5;
+
+        // Cek apakah hari adalah hari Senin
+        if (hari == 1) {
+            System.out.println("Hari ini adalah hari Senin");
+        } else if (hari == 2) {
+            System.out.println("Hari ini adalah hari Selasa");
+        } else if (hari == 3) {
+            System.out.println("Hari ini adalah hari Rabu");
+        } else if (hari == 4) {
+            System.out.println("Hari ini adalah hari Kamis");
+        } else if (hari == 5) {
+            System.out.println("Hari ini adalah hari Jumat");
+        } else if (hari == 6) {
+            System.out.println("Hari ini adalah hari Sabtu");
+        } else if (hari == 7) {
+            System.out.println("Hari ini adalah hari Minggu");
+        } else {
+            System.out.println("Hari tidak dikenal");
+        }
+    }
+    
+}
