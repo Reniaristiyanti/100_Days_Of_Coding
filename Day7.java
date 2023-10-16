25df9eff2717db5a8ccf3d096b7b5033286e5f35
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-16 21:34:47 +0800

    Day7

--- Day7.java
+++ Day7.java
@@ -8,15 +8,15 @@ public class Day7 {
         Scanner sc = new Scanner(System.in);
         
         String Nama ;
-        int umur;
+        int Umur;
         char Jeniskelamin;
         
         System.out.print("Masukkan nama: ");
         Nama = sc.nextLine();
         System.out.println("Nama saya adalah:" + Nama);
         System.out.print("Masukkan umur: ");
-        umur = sc.nextInt();
-        System.out.println("Umur saya adalah:" + umur + "Tahun");
+        Umur = sc.nextInt();
+        System.out.println("Umur saya adalah:" + Umur + "Tahun");
         System.out.print("Masukkan Jenis kelamin: ");
         Jeniskelamin = sc.next().charAt(0);
         System.out.println("Jenis kelamin saya: " + Jeniskelamin);
