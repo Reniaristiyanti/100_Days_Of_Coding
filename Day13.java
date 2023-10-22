7a9df52e35e7b6666c0e39394d105b726030ef0b
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-22 19:02:02 +0800

    Menampilkan angka 23 sampai dengan 170 menggunakan perulangan for

--- Day13.java
+++ Day13.java
@@ -3,7 +3,6 @@ package ISC;
 public class Day13 {
     public static void main(String[] args) {
         //Menampilkan angka mulai dari 23 sampai dengan 170 menggunakan perulangan for
-        int angka = 23;
         for (int i = 23; i <= 170; i++) {
             
         }
