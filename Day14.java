3aad1a72356bdb44898e60c7c48e4b7031fc73ee
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-22 19:08:29 +0800

    Menampilkan angka 100 ke 1 menggunakan perulangan while

---
+++ Day14.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class Day14 {
+    public static void main(String[] args) {
+        //Menampilkan angka 100 sampai 1 menggunakan perulangan while
+        int angka = 100;
+        while(angka >= 1){
+            System.out.println(angka --);
+        }
+    }
+    
+}
