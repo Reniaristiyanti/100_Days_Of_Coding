bbcf2a295f7d9b7d21b0ef295e5e35c6b84b3743
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-24 22:33:58 +0800

    Perulangan while

---
+++ Day15.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class Day15 {
+    public static void main(String[] args) {
+        int angka = 23;
+        while(angka <= 200){
+            System.out.println(angka);
+            angka +=5;
+        }
+    }
+    
+}
