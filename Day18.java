4698ff93e590ed37f7dd26ce5096abfba71ac388
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-27 16:39:30 +0800

    Perkalian perulangan while

---
+++ Day18.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class Day18 {
+    public static void main(String[] args) {
+        int hasil = 1;
+        int i = 1;
+        while (i <= 10) {
+            hasil *= i;
+            i++;
+        }
+        System.out.println("Hasil perkalian: " + hasil);
+    }
+    
+}
