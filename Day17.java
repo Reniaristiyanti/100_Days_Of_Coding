037824a01db83901cb776b2eca82d325467fdc42
Reniaristiyanti <renispcl51pro@gmail.com>
2023-10-27 16:37:40 +0800

    Penjumlahan perulangan for

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package ISC;
+
+public class Day17 {
+    public static void main(String[] args) {
+        int hasil = 0;
+        for (int i = 1; i <= 15; i++) {
+            hasil += i;
+            
+        }
+        System.out.println("Hasil penambahan: "+ hasil);
+    }
+    
+}
