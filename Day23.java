5a88249ab5cb7ef32cfae1b80390b179a471d81b
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-02 17:04:23 +0800

    Percabangan Switch-Case menggunakan inputan

---
+++ Day23.java
@@ -0,0 +1,26 @@
+package ISC;
+
+public class Day23 {
+    public static void main(String[] args) {
+        String nama = "Reni";
+        
+        switch(nama){
+            case "A":
+                System.out.println("Anda memilih nama lisa");
+                break;
+            case "B":
+                System.out.println("Anda memilih nama Rahma");
+                break;
+            case "C":
+                System.out.println("Anda memilih nama Reni");
+                break;
+            case "D":
+                System.out.println("Anda memilih nama Ica");
+            default:
+                System.out.println("Pilihan valid");
+       
+        }
+        
+    }
+    
+}
