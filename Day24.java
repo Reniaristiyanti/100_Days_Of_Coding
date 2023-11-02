ea0b4005e143b07477f6f4d7c4140df58288ca71
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-02 17:05:50 +0800

    ClassObject

---
+++ Day24.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day24 {
+    String nama;
+    String warna;
+    int jumlahBuah;
+    double berat;
+    
+    void cetak(){
+        System.out.println("Nama " + nama);
+        System.out.println("Warna "+ warna);
+        System.out.println("Jumlah buah "+ jumlahBuah);
+        System.out.println("Berat "+ berat);
+    }
+    
+}
