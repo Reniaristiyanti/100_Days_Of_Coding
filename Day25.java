33a24aae09bbabb2e6e53b8ca64621aa4284ead3
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-03 23:15:40 +0800

    Perulangan do while

---
+++ Day25.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class Day25 {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        do {
+            System.out.println("Indeks ke-" +i);
+            i++;
+        } while (i <= 5);
+    }
+    
+
+}
