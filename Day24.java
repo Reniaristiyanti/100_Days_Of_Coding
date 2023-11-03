3beaf63ef1b2c191c1032657b20a84f94a0fd5aa
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-03 23:13:54 +0800

    perulangan do while menggunakan scanner

--- Day24.java
+++ Day24.java
@@ -1,16 +1,28 @@
 package ISC;
 
+import java.util.Scanner;
+
 public class Day24 {
-    String nama;
-    String warna;
-    int jumlahBuah;
-    double berat;
-    
-    void cetak(){
-        System.out.println("Nama " + nama);
-        System.out.println("Warna "+ warna);
-        System.out.println("Jumlah buah "+ jumlahBuah);
-        System.out.println("Berat "+ berat);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char jawaban;
+        
+        do{
+            System.out.print("Apakah anda ingin mengulang (y/n)");
+            jawaban = sc.next().charAt(0);
+            
+            if (jawaban == 'y'|| jawaban == 'Y'){
+                System.out.println("Perulangan dilanjutkan.");
+        }else if (jawaban =='n'|| jawaban == 'N'){
+                System.out.print("Prulangan dihentikan.");
+        }else{
+                System.out.print("Maukan tidak valid. Silahkan masukkan 'y' atau 'n'.");
+        }
+            
+         
+        }while (jawaban != 'n' && jawaban != 'N');
+        
+        
     }
     
 }
