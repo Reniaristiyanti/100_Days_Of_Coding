994a317cd82cefc5b64c01a281f8604d5b35abed
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-06 23:11:09 +0800

    Perulangan continue

---
+++ Day27.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day27 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i==3){
+                System.out.println("Menghentikan perulangan ketika i = 3");
+                continue;
+            }
+            System.out.println("Nilai i:");
+            
+        }
+    
+    }
+    
+}
