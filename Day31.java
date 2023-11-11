a4a2711eed4eaba0f72bb78933c4d04588745e3d
Reniaristiyanti <renispcl51pro@gmail.com>
2023-11-12 00:04:51 +0800

    Perulangan for-Each

---
+++ Day31.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day31 {
+    public static void main(String[] args) {
+        int [] angka = {1,2,3,4,5,6,7,8,9,10};
+        for (int i : angka ) {
+            System.out.println(i);
+        }
+    }
+    
+}
