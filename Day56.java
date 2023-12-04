cc793a046ca6a929e8de0aab44c2ac95d1605415
Reniaristiyanti <renispcl51pro@gmail.com>
2023-12-04 13:32:45 +0800

    Increment di perulangan for

---
+++ Day56.java
@@ -0,0 +1,19 @@
+package ISC;
+
+public class Day56 {
+    public static void main(String[] args) {
+        System.out.println("Menggunakan increment dalam perulangan for: ");
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nilai i: "+ i);
+            
+        }
+        System.out.println("\nMenggunakan increment yang berbeda dalam perulangan for: ");
+        
+        for (int j = 0; j <= 10; j+=2) {
+            System.out.println("Nilai j" + j);
+            
+        }
+    }
+    
+}
