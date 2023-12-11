5a2ddeb291a2c4ea1b4b6afe39830196b87e0bbb
Reniaristiyanti <renispcl51pro@gmail.com>
2023-12-11 20:58:53 +0800

    Menampilkan item dengan perulangan di array

---
+++ Day58.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class Day58 {
+    public static void main(String[] args) {
+        String [] items = {"Item 1", "Item 2", "Item 3" , "Item 4", "Item 5"};
+        
+        for (String item : items ) {
+            System.out.println(item);
+        }
+    }
+    
+}
