b5776045502c87b18ee60b0b9bae0d0d7e893ff1
Reniaristiyanti <renispcl51pro@gmail.com>
2023-12-11 21:07:11 +0800

    Penggunaan class arrays untuk melakukan sorting

---
+++ Day61.java
@@ -0,0 +1,14 @@
+package ISC;
+
+import java.util.Arrays;
+
+public class Day61 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 6};
+
+        Arrays.sort(numbers);
+
+        System.out.println("Array setelah diurutkan: " + Arrays.toString(numbers));
+    }
+    
+}
