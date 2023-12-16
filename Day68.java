25075323ab7f1d99ed9a2fcccc205259b9e6952c
Reniaristiyanti <renispcl51pro@gmail.com>
2023-12-16 22:23:59 +0800

    while if else

---
+++ Day68.java
@@ -0,0 +1,25 @@
+package ISC;
+
+import java.util.Scanner;
+
+public class Day68 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("nama: ");
+            String nama = scanner.nextLine();
+
+            if (nama.equals("")) {
+                System.out.println("kosong");
+            } else if (nama.equals("selesai")) {
+                break;
+            } else {
+                System.out.println("nama saya: " + nama);
+            }
+        }
+
+        scanner.close();
+    }
+    
+}
