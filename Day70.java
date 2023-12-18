6b27a18c621cf4371c38cf59c3e9b6564d68bed5
Reniaristiyanti <renispcl51pro@gmail.com>
2023-12-19 00:05:40 +0800

    Array

---
+++ Day70.java
@@ -0,0 +1,17 @@
+package ISC;
+
+public class Day70 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int jumlahGanjil = 0;
+
+        for (int num : array) {
+            if (num % 2 == 1) {
+                jumlahGanjil++;
+            }
+        }
+
+        System.out.println("Jumlah Angka Ganjil: " + jumlahGanjil);
+    }
+    
+}
